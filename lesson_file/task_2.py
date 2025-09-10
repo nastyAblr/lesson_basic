47655141fc96ed3398142dec4e0fd45e61a05cb0
blr <britta18britta@gmail.com>
2025-09-10 22:53:55 +0300

    10.09.2025

---
+++ lesson_file/task_2.py
@@ -0,0 +1,12 @@
+"""
+Напишите программу, чтобы оценить, закрыт файл или нет.
+"""
+
+my_file = open('D:/dok.txt', 'a+', encoding=None)
+my_file.write('Я живу в городе Солигорск')
+
+print(my_file)
+print(my_file.closed)
+my_file.close()
+
+
